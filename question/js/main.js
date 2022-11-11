922ce00fe51d28a48cf0afa437e59c809e85db28
haya <66757856+haya-44@users.noreply.github.com>
2022-11-12 05:43:19 +0900

    Add files via upload

---
+++ question/js/main.js
@@ -0,0 +1,11 @@
+$(".menu-btn").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass("active"); //ボタン自身に activeクラスを付与し
+  $("#g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#header__g-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".menu-btn").removeClass("active"); //ボタンの activeクラスを除去し
+  $("#g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスも除去
+});
